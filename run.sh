9521242fe227db0b0b176395c2a9b72ac50d443e
zhuocai <jones.zhuo.cai@outlook.com>
2020-12-15 17:09:47 +0800

    finish writing

---
+++ run.sh
@@ -0,0 +1,13 @@
+data_folder=../../data/q1_data
+for i in 2 3 4 5
+do
+  for j in 1 2
+  do
+    python main.py --epochs 20 --tmax 20 --transform $i --task $j --root_dir $data_folder
+  done
+done
+
+for j in 1 2
+do
+  python main.py --epochs 100 --tmax 100 --transform $i --task $j --root_dir $data_folder
+done
